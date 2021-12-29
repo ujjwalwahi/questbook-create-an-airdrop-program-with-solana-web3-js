71224ec25db90412e8134a58876570f42db74279
Ujjwal Wahi <w.ujjwal@gmail.com>
2021-12-29 17:51:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    Transaction,
+    Account,
+} = require("@solana/web3.js");
+
+const newPair = new Keypair();
+const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+const secretKey = newPair._keypair.secretKey;
+
+console.log(publicKey);
+console.log(secretKey);
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`Wallet balance: ${walletBalance}`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const driverFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    await getWalletBalance();
+}
+
+driverFunction();
